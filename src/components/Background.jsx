507265b867d80cb1210c6aec1d3ce03c46524473
dlorr <hyesrold@gmail.com>
2026-02-02 21:45:35 +0800

    feaure - fix navbar and modify color theme

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,12 +1,12 @@
 import Meteors from "./ui/Meteors";
 import StarsBackground from "./ui/StarsBackground";
-import ShootingStars from "./ui/ShootingStars";
+// import ShootingStars from "./ui/ShootingStars";
 
 export default function StarBackground() {
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
       <StarsBackground />
-      <ShootingStars />
+      {/* <ShootingStars /> */}
       <Meteors number={10} />
     </div>
   );
