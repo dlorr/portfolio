4e0452050134412bd5096566042d0d2625214538
dlorr <hyesrold@gmail.com>
2026-02-19 17:56:25 +0800

    feature - update project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,23 @@
 import project1 from "@/assets/images/projects/project1.png";
+import aegis from "@/assets/images/projects/aegis.png";
 
 export const projects = [
   {
     id: 1,
-    title: "Project 1",
+    title: "Aegis",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam hic eos illum officia.",
-    image: project1,
-    tags: ["React.js", "Tailwind CSS", "Express.js"],
+      "A full-stack JWT authentication system built with React, TypeScript, Express, and MongoDB. Features secure login, registration, email verification, password recovery, and multi-device session management.",
+    image: aegis,
+    tags: [
+      "React.js",
+      "Express.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "MongoDB",
+      "JWT",
+    ],
     demoUrl: "#",
-    githubUrl: "#",
+    githubUrl: "https://github.com/dlorr/jwt-auth-login",
   },
   {
     id: 2,
